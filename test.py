27a6508bb117f6bb8dbe41fa6b5073d456a2cd65
Allen Calderon <ACalderon01@indianatech.net>
2025-10-06 14:07:51 -0400

    Test connecting to github

---
+++ test.py
@@ -0,0 +1,10 @@
+print("hello")
+print("ur fat boy")
+movie= "independence day"
+address="1600 e washington blvd fort wayne indiana 46803"
+print(movie)
+print(address)
+wife= "adriana cabello"
+print(wife)
+tournament= "2026 NAIA national champ at 125 pounds.... Allen Calderon-peterson"
+print(tournament)
